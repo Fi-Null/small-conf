58a3ca04c2322553f8d5f87bc66f024279c08460
向科 <xiangke6@jd.com>
2019-12-27 11:27:06 +0800

    init

---
+++ small-config-admin/src/main/java/com/small/config/admin/SmallConfigAdminApplication.java
@@ -0,0 +1,13 @@
+package com.small.config.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallConfigAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallConfigAdminApplication.class, args);
+    }
+
+}
---
+++ small-config-admin/src/test/java/com/small/config/admin/SmallConfigAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.small.config.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallConfigAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
